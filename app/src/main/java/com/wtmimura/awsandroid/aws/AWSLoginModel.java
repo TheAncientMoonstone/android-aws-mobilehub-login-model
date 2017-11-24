c6bba2e9692459f688384cbfeb727fabe0449dce
williamtmimura <mimurawil@gmail.com>
2017-11-23 23:34:08 -0800

    Adding some comments. Saving user attributes in SharedPreferences.

--- app/src/main/java/com/wtmimura/awsandroid/aws/AWSLoginModel.java
+++ app/src/main/java/com/wtmimura/awsandroid/aws/AWSLoginModel.java
@@ -1,3 +1,13 @@
+/*
+ * Author: William Takashi Mimura
+ * Date: November 23, 2017
+ * Website: www.wtmimura.com
+ * Repository: https://github.com/mimurawil/android-aws-mobilehub-login-model
+ *
+ * You can copy, change, or whatever. Just use some common sense and good faith.
+ * If you use this, please leave a message at wotom.wtmimura@gmail.com. That'd be awesome!
+ */
+
 package com.wtmimura.awsandroid.aws;
 
 import android.content.Context;
@@ -8,6 +18,7 @@ import com.amazonaws.mobileconnectors.cognitoidentityprovider.CognitoDevice;
 import com.amazonaws.mobileconnectors.cognitoidentityprovider.CognitoUser;
 import com.amazonaws.mobileconnectors.cognitoidentityprovider.CognitoUserAttributes;
 import com.amazonaws.mobileconnectors.cognitoidentityprovider.CognitoUserCodeDeliveryDetails;
+import com.amazonaws.mobileconnectors.cognitoidentityprovider.CognitoUserDetails;
 import com.amazonaws.mobileconnectors.cognitoidentityprovider.CognitoUserPool;
 import com.amazonaws.mobileconnectors.cognitoidentityprovider.CognitoUserSession;
 import com.amazonaws.mobileconnectors.cognitoidentityprovider.continuations.AuthenticationContinuation;
@@ -16,16 +27,36 @@ import com.amazonaws.mobileconnectors.cognitoidentityprovider.continuations.Chal
 import com.amazonaws.mobileconnectors.cognitoidentityprovider.continuations.MultiFactorAuthenticationContinuation;
 import com.amazonaws.mobileconnectors.cognitoidentityprovider.handlers.AuthenticationHandler;
 import com.amazonaws.mobileconnectors.cognitoidentityprovider.handlers.GenericHandler;
+import com.amazonaws.mobileconnectors.cognitoidentityprovider.handlers.GetDetailsHandler;
 import com.amazonaws.mobileconnectors.cognitoidentityprovider.handlers.SignUpHandler;
 import com.amazonaws.regions.Regions;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
+/**
+ * This represents a model for login operations on AWS Mobile Hub. It manages login operations
+ * such as:
+ * - Sign In
+ * - Sign Up
+ * - Confirm Sign Up
+ * - Resend Confirmation Code
+ * - Recover Password
+ * - Change Password
+ * - Sign Out
+ * - Delete Account
+ * - Get User Name (current signed in)
+ * - Get User E-mail (current signed in)
+ *
+ */
+@SuppressWarnings("unused")
 public class AWSLoginModel {
 
     // constants
-    public static final String ATTR_EMAIL = "email";
+    private final String ATTR_EMAIL = "email";
+    private static final String SHARED_PREFERENCE = "SavedValues";
+    private static final String PREFERENCE_USER_NAME = "awsUserName";
+    private static final String PREFERENCE_USER_EMAIL = "awsUserEmail";
     public static final int PROCESS_SIGN_IN = 1;
     public static final int PROCESS_REGISTER = 2;
     public static final int PROCESS_CONFIRM_REGISTRATION = 3;
@@ -42,8 +73,26 @@ public class AWSLoginModel {
     private final AuthenticationHandler authenticationHandler = new AuthenticationHandler() {
         @Override
         public void onSuccess(CognitoUserSession userSession, CognitoDevice newDevice) {
-            SharedPreferences.Editor editor = mContext.getSharedPreferences("SavedValues", Context.MODE_PRIVATE).edit();
-            editor.putString("userName", userName);
+            // Get details of the logged user (in this case, only the e-mail)
+            mCognitoUser.getDetailsInBackground(new GetDetailsHandler() {
+                @Override
+                public void onSuccess(CognitoUserDetails cognitoUserDetails) {
+                    // Save in SharedPreferences
+                    SharedPreferences.Editor editor = mContext.getSharedPreferences(SHARED_PREFERENCE, Context.MODE_PRIVATE).edit();
+                    String email = cognitoUserDetails.getAttributes().getAttributes().get(ATTR_EMAIL);
+                    editor.putString(PREFERENCE_USER_EMAIL, email);
+                    editor.apply();
+                }
+
+                @Override
+                public void onFailure(Exception exception) {
+                    exception.printStackTrace();
+                }
+            });
+
+            // Save in SharedPreferences
+            SharedPreferences.Editor editor = mContext.getSharedPreferences(SHARED_PREFERENCE, Context.MODE_PRIVATE).edit();
+            editor.putString(PREFERENCE_USER_NAME, userName);
             editor.apply();
             mCallback.onSignInSuccess();
         }
@@ -100,9 +149,11 @@ public class AWSLoginModel {
     /**
      * Registers new user to the AWS Cognito User Pool.
      *
-     * @param userName          REQUIRED: Username to be registered. Must be unique in the User Pool.
-     * @param userEmail         REQUIRED: E-mail to be registered. Must be unique in the User Pool.
-     * @param userPassword      REQUIRED: Password of this new account.
+     * This will trigger {@link AWSLoginHandler} interface defined when the constructor was called.
+     *
+     * @param userName         REQUIRED: Username to be registered. Must be unique in the User Pool.
+     * @param userEmail        REQUIRED: E-mail to be registered. Must be unique in the User Pool.
+     * @param userPassword     REQUIRED: Password of this new account.
      *
      */
     public void registerUser(String userName, String userEmail, String userPassword) {
@@ -123,11 +174,12 @@ public class AWSLoginModel {
         };
 
         mCognitoUserPool.signUpInBackground(userName, userPassword, userAttributes, null, signUpHandler);
-
     }
 
     /**
      * Confirms registration of the new user in AWS Cognito User Pool.
+     *
+     * This will trigger {@link AWSLoginHandler} interface defined when the constructor was called.
      *
      * @param confirmationCode      REQUIRED: Code sent from AWS to the user.
      */
@@ -148,7 +200,10 @@ public class AWSLoginModel {
     }
 
     /**
-     * Sign in process.
+     * Sign in process. If succeeded, this will save the user name and e-mail in SharedPreference of
+     * this context.
+     *
+     * This will trigger {@link AWSLoginHandler} interface defined when the constructor was called.
      *
      * @param userNameOrEmail        REQUIRED: Username or e-mail.
      * @param userPassword           REQUIRED: Password.
@@ -161,5 +216,25 @@ public class AWSLoginModel {
         mCognitoUser.getSessionInBackground(authenticationHandler);
     }
 
+    /**
+     * Gets the user name saved in SharedPreferences.
+     *
+     * @param context               REQUIRED: Android application context.
+     * @return                      user name saved in SharedPreferences.
+     */
+    public static String getSavedUserName(Context context) {
+        SharedPreferences savedValues = context.getSharedPreferences(SHARED_PREFERENCE, Context.MODE_PRIVATE);
+        return savedValues.getString(PREFERENCE_USER_NAME, "");
+    }
 
+    /**
+     * Gets the user e-mail saved in SharedPreferences.
+     *
+     * @param context               REQUIRED: Android application context.
+     * @return                      user e-mail saved in SharedPreferences.
+     */
+    public static String getSavedUserEmail(Context context) {
+        SharedPreferences savedValues = context.getSharedPreferences(SHARED_PREFERENCE, Context.MODE_PRIVATE);
+        return savedValues.getString(PREFERENCE_USER_EMAIL, "");
+    }
 }
